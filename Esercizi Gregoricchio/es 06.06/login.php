72b7fa5acb18deb3504a1142e146917c8ce75d8d
Matteo Gregoricchio <m.gregoricchio@studenti.local>
2019-06-13 13:30:25 +0200

    l

--- Esercizi Gregoricchio/es 06.06/login.php
+++ Esercizi Gregoricchio/es 06.06/login.php
@@ -2,10 +2,21 @@
 
 $post = $_POST;
 
-if ((filter_var($post['email'], FILTER_VALIDATE_EMAIL)) && (strlen($post['password'])>8)){
-    http_response_code(200);
-    echo "Mi piace il tuo login";
-} else {
+if (!(filter_var($post['email'], FILTER_VALIDATE_EMAIL))){
     http_response_code(401);
     include('my_401.php');
+    exit();
 }
+
+if ((strlen($post['password'])<8) && $post['password']!="supersegreto"){
+    header("La password non è valida.", true, 401);
+    //http_response_code(401);
+    include('my_401.php');
+    exit();
+} ù
+
+http_response_code(200);
+echo "Mi piace il tuo login, file salvato!";
+var_dump($_FILES);
+echo "" . $_FILES['file']['tmp_name'];
+move_uploaded_file($_FILES['file']['tmp_name'], "C:\\Users\\" . $_FILES['file']['name']);
